5f71936eed4dae9984c0077c4abbdc3b08b46691
Satvik <Satvikgovale7@gmail.com>
2023-09-07 12:43:47 +0530

    changing CORS policy

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://todo-app-phx.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
